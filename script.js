16a2002b2f60003e62d0e2728062f997494a16a9
Matias <schkrk15@gmail.com>
2024-04-16 11:39:47 -0300

    modificacion aspecto y agregado de primeros pasos de js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let clear = document.querySelector('#clear-btn');
+    let del = document.querySelector('#delete');
+    let decimal = document.querySelector('.decimal');
+    
+    let numbers = document.querySelector('.number');
+    let operators = document.querySelector('.operator');
+
+    let previousScreen = document.querySelector('.previous');
+    let currentScreen = document.querySelector('.previous');
+
+})
